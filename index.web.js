51af43b11748e3c683822a2fb6283181817007f7
Dmitriy Shishkin <dshishkin@allmax.team>
2018-12-12 11:18:38 +0700

    Fix hot reload

--- index.web.js
+++ index.web.js
@@ -3,32 +3,12 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
 
-import { AppContainer } from 'react-hot-loader';
+import { hot } from 'react-hot-loader'
 
 import App from './src';
 
-const renderApp = () => (
-  <AppContainer>
-    <App />
-  </AppContainer>
-);
-
-AppRegistry.registerComponent('ReactNativeWebBoilerplate', () => renderApp);
-
-// $FlowFixMe
-if (module.hot) {
-  // $FlowFixMe
-  module.hot.accept();
-
-  const renderHotApp = () => (
-    <AppContainer>
-      <App />
-    </AppContainer>
-  );
-
-  // App registration and rendering
-  AppRegistry.registerComponent('ReactNativeWebBoilerplate', () => renderHotApp);
-}
+// App registration and rendering
+AppRegistry.registerComponent('ReactNativeWebBoilerplate', () => hot(module)(App));
 
 AppRegistry.runApplication('ReactNativeWebBoilerplate', {
   rootTag: document.getElementById('root'),
