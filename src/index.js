1354d3fc402d78be7d63d0f9e5c0db00f8b95075
Dmitriy Shishkin <dshishkin@allmax.team>
2018-12-12 09:49:34 +0700

    Render table

--- src/index.js
+++ src/index.js
@@ -14,17 +14,10 @@ class MainScreen extends React.Component<{}> {
         <StatusBar backgroundColor="blue" barStyle="light-content" />
         <Banner />
         <MarkdownView>
-          # MarkdownView{'\n'}
-          {'\n'}
-          **React Native** is even better with Markdown!{'\n'}
-          {'\n'}
-          ![RN Logo](https://reactjs.org/logo-og.png =120x63){'\n'}
-          {'\n'}
-          `react-native-markdown-view` is:{'\n'}
-          {'\n'}
-          * Easy to use{'\n'}
-          * Looks good by default{'\n'}
-          * Is __extensible__{'\n'}
+          Markdown | Less | Pretty{'\n'}
+          --- | --- | ---{'\n'}
+          *Still* | `renders` | **nicely**{'\n'}
+          1 | 2 | 3{'\n'}
         </MarkdownView>
       </ScrollView>
     );
