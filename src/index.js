a6b75a5dedd6c8796aab08933b0cbe2630aa7412
Dmitriy Shishkin <dshishkin@allmax.team>
2018-12-12 10:23:11 +0700

    Add more markdown

--- src/index.js
+++ src/index.js
@@ -22,6 +22,17 @@ class MainScreen extends React.Component<{}> {
         <MarkdownView
           onLinkPress={this.onLinkPress}
         >
+          # MarkdownView{'\n'}
+          {'\n'}
+          **React Native** is even better with Markdown!{'\n'}
+          {'\n'}
+          ![RN Logo](https://reactjs.org/logo-og.png =120x63){'\n'}
+          {'\n'}
+          `react-native-markdown-view` is:{'\n'}
+          {'\n'}
+          * Easy to use{'\n'}
+          * Looks good by default{'\n'}
+          * Is __extensible__{'\n'}{'\n'}
           Markdown | Less | Pretty{'\n'}
           --- | --- | ---{'\n'}
           *Still* | `renders` | **nicely**{'\n'}
