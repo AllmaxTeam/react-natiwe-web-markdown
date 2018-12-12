092333c3893121f1e5d7e147a307c05aecb319bf
Dmitriy Shishkin <dshishkin@allmax.team>
2018-12-12 10:02:24 +0700

    Add onLinkPress

--- src/index.js
+++ src/index.js
@@ -2,22 +2,31 @@
 
 import React from 'react';
 
-import { ScrollView, View, StatusBar } from 'react-native';
+import { ScrollView, View, StatusBar, Linking } from 'react-native';
 import { MarkdownView } from 'react-native-markdown-view'
 
 import Banner from './Banner';
 
 class MainScreen extends React.Component<{}> {
+  onLinkPress = (url: string) => {
+    Linking.openURL(url).catch((error) => {
+      console.warn('An error occurred: ', error);
+    });
+  };
+
   render() {
     return (
       <ScrollView>
         <StatusBar backgroundColor="blue" barStyle="light-content" />
         <Banner />
-        <MarkdownView>
+        <MarkdownView
+          onLinkPress={this.onLinkPress}
+        >
           Markdown | Less | Pretty{'\n'}
           --- | --- | ---{'\n'}
           *Still* | `renders` | **nicely**{'\n'}
           1 | 2 | 3{'\n'}
+          [react-native-markdown-view](https://github.com/Benjamin-Dobell/react-native-markdown-view/)
         </MarkdownView>
       </ScrollView>
     );
